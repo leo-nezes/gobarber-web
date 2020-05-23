e9279a7afb276ef734673333a9833dc70d8b338a
Leonardo Menezes <leonhard.menezes@gmail.com>
2020-05-23 14:57:23 -0300

    Bootcamp GoStack - Renomeando pastas de context para hooks - Criando hook useAuth() - Criando logout da aplicação

--- src/hooks/AuthContext.tsx
+++ src/hooks/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useCallback, useState } from 'react';
+import React, { createContext, useCallback, useState, useContext } from 'react';
 import api from '../services/api';
 
 interface AuthState {
@@ -13,11 +13,12 @@ interface SignInCredentials {
 interface AuthContextData {
   user: object;
   signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
 }
 
-export const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
-export const AuthProvider: React.FC = ({ children }) => {
+const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>(() => {
     const token = localStorage.getItem('@GoBarber:token');
     const user = localStorage.getItem('@GoBarber:user');
@@ -41,11 +42,28 @@ export const AuthProvider: React.FC = ({ children }) => {
       localStorage.setItem('@GoBarber:user', JSON.stringify(user));
 
       setData({ token, user });
-  }, [])
+  }, []);
+
+  const signOut = useCallback(() => {
+    localStorage.removeItem('@GoBarber:token');
+    localStorage.removeItem('@GoBarber:user');
+
+    setData({} as AuthState);
+  }, []);
 
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
 };
+
+function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+
+  if(!context) throw new Error('useAuth must be used within an AuthProvider');
+
+  return context;
+}
+
+export { AuthProvider, useAuth };
