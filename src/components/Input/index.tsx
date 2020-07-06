f5f7200f3a13dfba1f3ff7b7fa9d5153fa7cef79
Leonardo Menezes <leonhard.menezes@gmail.com>
2020-07-06 18:28:39 -0300

    [Bootcamp GoStack] Perfil de usuário - Desativando regra ban-types do eslint - Adicionando nova rota /profile - Adicionando novo atributo no componente Input - Tornando o email acessível através do hook useAuth - Criando e estilizando página Profile

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,16 +1,28 @@
-import React, { InputHTMLAttributes, useEffect, useRef, useState, useCallback } from 'react';
-import { IconBaseProps } from 'react-icons'
-import { FiAlertCircle } from 'react-icons/fi'
-import { useField } from '@unform/core'
+import React, {
+  InputHTMLAttributes,
+  useEffect,
+  useRef,
+  useState,
+  useCallback,
+} from 'react';
+import { IconBaseProps } from 'react-icons';
+import { FiAlertCircle } from 'react-icons/fi';
+import { useField } from '@unform/core';
 
 import { Container, Error } from './styles';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-    name: string;
-    icon?: React.ComponentType<IconBaseProps>;
+  name: string;
+  containerStyle?: object;
+  icon?: React.ComponentType<IconBaseProps>;
 }
 
-const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+const Input: React.FC<InputProps> = ({
+  name,
+  containerStyle = {},
+  icon: Icon,
+  ...rest
+}) => {
   const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
@@ -19,7 +31,7 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
   const { fieldName, defaultValue, error, registerField } = useField(name);
 
   const handleInputFocus = useCallback(() => {
-    setIsFocused(true)
+    setIsFocused(true);
   }, []);
 
   const handleInputBlur = useCallback(() => {
@@ -33,26 +45,32 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
       name: fieldName,
       ref: inputRef.current,
       path: 'value',
-    })
+    });
   }, [fieldName, registerField]);
 
   return (
-    <Container isErrored={!!error} isFilled={isFilled} isFocused={isFocused}>
-      { Icon && <Icon size={20} /> }
+    <Container
+      style={containerStyle}
+      isErrored={!!error}
+      isFilled={isFilled}
+      isFocused={isFocused}
+    >
+      {Icon && <Icon size={20} />}
       <input
         onFocus={handleInputFocus}
         onBlur={handleInputBlur}
         defaultValue={defaultValue}
         ref={inputRef}
-        { ...rest} />
+        {...rest}
+      />
 
       {error && (
         <Error title={error}>
-          <FiAlertCircle color='#c53030' size={20}/>
+          <FiAlertCircle color="#c53030" size={20} />
         </Error>
       )}
     </Container>
   );
-}
+};
 
 export default Input;
